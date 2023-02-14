12a034b58205cbda656d8b846a516d1eedb0e612
aboongm <aboongm@yahoo.com>
2023-02-14 21:54:01 +0530

    Add changes

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,47 +1,3 @@
-// import { Controller } from '@hotwired/stimulus';
-// import { leave, toggle } from 'el-transition';
-
-// export default class extends Controller {
-//   // static targets = ['openUserMenu', 'userAuthLink'];
-//   static targets = ['userAuthLink'];
-
-//   connect() {
-//     // this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu);
-        
-//     this.userAuthLinkTargets.forEach((link) => {
-//       link.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         console.log('Header controller connected');
-//         document.getElementById('user-auth-modal-trigger').click();
-//         // document.getElementById('modal-wrapper').click();
-//       });
-//     });
-
-//     const header = document.getElementById('myHeader');
-//     const sticky = header.offsetTop;
-
-//     myFunction(header, sticky) {
-//       if (window.pageYOffset > sticky) {
-//         header.classList.add("sticky");
-//       } else {
-//         header.classList.remove("sticky");
-//       }
-//     }
-//   }
-  
-//   toggleDropdownMenu() {
-//     toggle(document.getElementById('menu-dropdown-items'));
-//   }
-  
-//   myFunction(header) {
-//     if (window.pageYOffset > sticky) {
-//       header.classList.add("sticky");
-//     } else {
-//       header.classList.remove("sticky");
-//     }
-//   }
-// }
-
 import { Controller } from '@hotwired/stimulus';
 import { toggle } from 'el-transition';
 
@@ -56,10 +12,14 @@ export default class extends Controller {
         document.getElementById('user-auth-modal-trigger').click();
       });
     });
-
+    
     const header = document.getElementById('header');
     const sticky = header.offsetTop;
-
+    const headerRect = header.getBoundingClientRect();
+    const modal = document.getElementById("menuList");
+    const modalTop = headerRect.bottom;
+    
+    // modal.style.top = modalTop + "px";
     window.onscroll = () => {
       this.myFunction(header, sticky)
     };
