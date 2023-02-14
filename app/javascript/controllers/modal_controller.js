9e098016a13e8e7cafe7c400a648f92f39788b7a
aboongm <aboongm@yahoo.com>
2023-02-14 20:29:09 +0530

    Implement On Scroll Sticky Header Add functionalities for popup modals of menu and login

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,61 @@
+import { Controller } from '@hotwired/stimulus';
+import { enter, leave } from 'el-transition';
+
+export default class extends Controller {
+  static targets = ['closeButton'];
+  connect() {
+    document
+      .getElementById(`modal-${this.element.dataset.modalTriggerId}-wrapper`)
+      .addEventListener('click', (event) => {
+        this.closeModal(event, this.element.dataset.modalTriggerId);
+      });
+    this.closeButtonTarget.addEventListener('click', () => {
+      leave(
+        document.getElementById(
+          `modal-${this.element.dataset.modalTriggerId}-wrapper`
+        )
+      );
+      leave(
+        document.getElementById(
+          `modal-${this.element.dataset.modalTriggerId}-backdrop`
+        )
+      );
+      leave(
+        document.getElementById(
+          `modal-${this.element.dataset.modalTriggerId}-panel`
+        )
+      );
+    });
+  }
+
+  closeModal(event, triggerId) {
+    const modalPanelClicked = document
+      .getElementById(`modal-${triggerId}-panel`)
+      .contains(event.target);
+
+    // if (!modalPanelClicked && event.target.id !== 'modal-trigger') {
+    if (!modalPanelClicked && event.target.id !== triggerId) {
+      leave(document.getElementById(`modal-${triggerId}-wrapper`));
+      leave(document.getElementById(`modal-${triggerId}-backdrop`));
+      leave(document.getElementById(`modal-${triggerId}-panel`));
+    }
+  }
+
+  showModal() {
+    enter(
+      document.getElementById(
+        `modal-${this.element.dataset.modalTriggerId}-wrapper`
+      )
+    );
+    enter(
+      document.getElementById(
+        `modal-${this.element.dataset.modalTriggerId}-backdrop`
+      )
+    );
+    enter(
+      document.getElementById(
+        `modal-${this.element.dataset.modalTriggerId}-panel`
+      )
+    );
+  }
+}
