796309916e5acc02670ccc71a8f082b83c2a32b4
aboongm <aboongm@yahoo.com>
2023-02-15 10:39:23 +0530

    Add changes to header menu and searchIcon

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,33 +1,52 @@
 import { Controller } from '@hotwired/stimulus';
-import { toggle } from 'el-transition';
+import { enter, leave, toggle } from 'el-transition';
 
 export default class extends Controller {
-  static targets = ['userAuthLink'];
+  static targets = ['openMenu', 'searchNews', 'closeMenu', 'userAuthLink'];
 
   connect() {
     this.userAuthLinkTargets.forEach((link) => {
       link.addEventListener('click', (e) => {
         e.preventDefault();
-        console.log('Header controller connected');
         document.getElementById('user-auth-modal-trigger').click();
+      });
+    });
+    
+    this.openMenuTargets.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        enter(document.getElementById('menuList'))
+      });
+    });
+    
+    this.searchNewsTargets.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        enter(document.getElementById('menuList'))
+      });
+    });
+
+    this.closeMenuTargets.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        leave(document.getElementById('menuList'))
       });
     });
     
     const header = document.getElementById('header');
     const sticky = header.offsetTop;
     const headerRect = header.getBoundingClientRect();
-    const modal = document.getElementById("menuList");
-    const modalTop = headerRect.bottom;
+    // const modal = document.getElementById("menuList");
+    // const modalTop = headerRect.bottom;
     
-    // modal.style.top = modalTop + "px";
     window.onscroll = () => {
       this.myFunction(header, sticky)
     };
   }
   
-  toggleDropdownMenu() {
-    toggle(document.getElementById('menu-dropdown-items'));
-  }
+  // toggleDropdownMenu() {
+  //   toggle(document.getElementById('menu-dropdown-items'));
+  // }
   
   myFunction(header, sticky) {
     if (window.pageYOffset > sticky) {
